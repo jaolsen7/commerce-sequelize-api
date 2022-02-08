fd2896abc0b9ec02be619a56aa5a3cfe9113f173
jaolsen7 <jaolsen@berkeley.edu>
2022-02-07 19:49:30 -0800

    completed product and tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,55 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product, through: ProductTag}]
+  }).then((allTagData) => {
+    res.json(allTagData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findbyPk(req.params.id, {
+    include: [{model: Product, through: ProductTag}]
+  }).then((oneTagData) => {
+    res.json(oneTagData);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then((newTag) => {
+    res.json(newTag)
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {id: req.params.id}
+    },
+  ).then((updatedTag) => {
+    res.json(updatedTag)
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((deleteData) => {
+    res.json(deleteData);
+  })
 });
 
 module.exports = router;
